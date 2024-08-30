3f6fad00ddb5ab94326c37455f96298feef025a8
s26842 <s26842@pjwstk.edu.pl>
2024-08-30 14:09:49 +0200

    Completed to pre release version

---
+++ src/app/_interfaces/login.model.ts
@@ -0,0 +1,8 @@
+export class LoginModel {
+  constructor(login: string, password: string) {
+    this.login = login;
+    this.password = password;
+  }
+  login: string;
+  password: string;
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AuthGuardService} from "./_service/auth-guard.service";
+import {AuthorizationComponent} from "./authorization/authorization.component";
+import {HomeComponent} from "./home/home.component";
+import {ImagesComponent} from "./images/images.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "login", component: AuthorizationComponent}, {
+    path: "register", component: AuthorizationComponent
+  },
+  {path: "images", component: ImagesComponent, canActivate: [AuthGuardService]},
+  {
+    path: "", component: HomeComponent, canActivate: [AuthGuardService],
+  }
+];
