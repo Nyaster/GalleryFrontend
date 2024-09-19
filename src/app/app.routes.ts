17b7d4966284dcaa7238a45cb3c6ced2c8c69699
s26842 <s26842@pjwstk.edu.pl>
2024-09-19 03:30:31 +0200

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,18 @@ import {AuthGuardService} from "./_service/auth-guard.service";
 import {AuthorizationComponent} from "./authorization/authorization.component";
 import {HomeComponent} from "./home/home.component";
 import {ImagesComponent} from "./images/images.component";
+import {ImagePageComponent} from "./images/image-page/image-page.component";
 
 export const routes: Routes = [
   {path: "login", component: AuthorizationComponent}, {
     path: "register", component: AuthorizationComponent
   },
-  {path: "images", component: ImagesComponent, canActivate: [AuthGuardService]},
   {
-    path: "", component: HomeComponent, canActivate: [AuthGuardService],
-  }
+    path: "images", component: ImagesComponent, canActivate: [AuthGuardService]
+  },
+  {path: "images/:id", component: ImagePageComponent},
+  {
+    path: "home", component: HomeComponent, canActivate: [AuthGuardService],
+  },
+  {path: "", redirectTo: "home", pathMatch: "full"}
 ];
